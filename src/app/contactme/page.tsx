72b76c758fa880da52c3f44b997e1001617ff90b
Papa Yaw Agyeman-Manu <agy20002@byui.edu>
2024-09-18 16:57:27 -0600

    contact page done

--- src/app/contactme/page.tsx
+++ src/app/contactme/page.tsx
@@ -1,7 +1,12 @@
+import ContactCard from "@/components/contactCard";
+
 export default function ContactMe() {
   return (
-    <div>
-      <h1>Contact Me</h1>
+    <div className="flex h-[100vh] w-full flex-col items-center justify-center">
+      <div className="space-y-4">
+        <h1>Contact Me - Let&apos;s talk about your next project</h1>
+        <ContactCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/contactCard.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export default function ContactCard() {
+  return (
+    <div className="space-y-4 rounded-xl border-2 border-sky-300 p-2 shadow-xl shadow-sky-300/20">
+      <div className="flex items-center justify-between">
+        <div className="relative h-[75px] w-[75px]">
+          <Image
+            src="https://utfs.io/f/b35332ed-4c66-4ced-9121-6a4d65fa729a-o9sf04...jpg"
+            alt="recommender image"
+            fill
+            style={{ objectFit: "cover" }}
+            className="rounded-full shadow-2xl shadow-sky-300/20"
+          />
+        </div>
+        <div>Papa Yaw&apos;s Contact Card</div>
+      </div>
+      <div>
+        <div>
+          Email:&nbsp;
+          <a
+            href="mailto:yawam0902@gmail.com"
+            className="border-b-2 border-sky-300"
+          >
+            yawam0902@gmail.com
+          </a>
+          <br />
+          Phone:&nbsp;
+          <a href="tel:+12089483868" className="border-b-2 border-sky-300">
+            208-948-3868
+          </a>
+          <br />
+        </div>
+      </div>
+    </div>
+  );
+}
